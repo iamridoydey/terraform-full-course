2c3dccc6b4584b908d1b76cb2584b1b50013d8cf
Ridoy Chandra Dey <iamridoydey@gmail.com>
2026-01-13 21:10:34 +0600

    Terraform azure function

---
+++ azure/9.function/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-demo"
+    storage_account_name = "tfstate10796"
+    container_name       = "tfstate"
+    key                  = "functions.terraform.tfstate"
+  }
+}
---
+++ azure/9.function/outputs.tf
@@ -0,0 +1,3 @@
+output "function_url" {
+  value = azurerm_function_app_function.greeting_func.url
+}
